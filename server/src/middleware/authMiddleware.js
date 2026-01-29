e25fb1e4064155f97ee909c7c026854541b08224
Aditya Agrawal <adityaagarwal2227@gmail.com>
2026-01-30 02:17:29 +0530

    debug: add server-side logging for auth middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -8,6 +8,7 @@ const protect = async (req, res, next) => {
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
+        console.log('Auth Middleware: Token found:', req.headers.authorization.substring(0, 20) + '...');
         try {
             token = req.headers.authorization.split(' ')[1];
 
@@ -23,6 +24,7 @@ const protect = async (req, res, next) => {
     }
 
     if (!token) {
+        console.log('Auth Middleware: No token found. Headers:', req.headers);
         res.status(401).json({ message: 'Not authorized, no token' });
     }
 };
